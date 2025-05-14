cac0b11fad3ab8df91acfa521163dd4471cdf9b3
tymothe blanc <tymotheblanc499@gmail.com>
2025-05-14 17:33:01 +0200

    ECO

--- Assets/Script/test.cs
+++ Assets/Script/test.cs
@@ -6,23 +6,11 @@ public class test : MonoBehaviour
 {
 
     vie BarreDeVie = new vie();
-    faim BarreDeFaim = new faim();
-    soif BarreDeSoif = new soif();
-    sexe BarreDeSexe = new sexe();
-
     void Start()
     {
         BarreDeVie.max = 100;
         BarreDeVie.valeur = 100;
 
-        BarreDeFaim.max = 100;
-        BarreDeFaim.valeur = 100;
-
-        BarreDeSoif.max = 100;
-        BarreDeSoif.valeur = 100;
-
-        BarreDeSexe.max = 100;
-        BarreDeSexe.valeur = 100;
     }
 
 
@@ -39,38 +27,5 @@ public class test : MonoBehaviour
 
         }
 
-        if (Input.GetKeyDown(KeyCode.Keypad1))
-        {
-            BarreDeFaim.valeur -= 10;
-
-        }
-        if (Input.GetKeyDown(KeyCode.Keypad2))
-        {
-            BarreDeFaim.valeur += 10;
-
-        }
-
-        if (Input.GetKeyDown(KeyCode.Keypad4))
-        {
-            BarreDeSoif.valeur -= 10;
-
-        }
-        if (Input.GetKeyDown(KeyCode.Keypad5))
-        {
-            BarreDeSoif.valeur += 10;
-
-        }
-
-
-        if (Input.GetKeyDown(KeyCode.Keypad7))
-        {
-            BarreDeSexe.valeur -= 10;
-
-        }
-        if (Input.GetKeyDown(KeyCode.Keypad8))
-        {
-            BarreDeSexe.valeur += 10;
-
-        }
     }
 }
